3a9a370a0f4609ac79133debbd8b376d2f1222ec
Andrés Galván <8il.andre@gmail.com>
2026-06-21 15:51:41 -0300

    fix: correct test security setup, switch to MockitoBean

--- core-api/src/test/kotlin/online/hanatalk/AuthControllerTest.kt
+++ core-api/src/test/kotlin/online/hanatalk/AuthControllerTest.kt
@@ -1,33 +1,37 @@
 package online.hanatalk
 
+import online.hanatalk.api.AuthController
 import online.hanatalk.api.dto.AuthResponse
 import online.hanatalk.security.JwtService
+import online.hanatalk.security.SecurityConfig
 import online.hanatalk.security.UserDetailsServiceImpl
 import online.hanatalk.service.AuthService
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.given
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
-import online.hanatalk.api.AuthController
 
-@WebMvcTest(AuthController::class)
+@WebMvcTest(AuthController::class, excludeAutoConfiguration = [UserDetailsServiceAutoConfiguration::class])
+@Import(SecurityConfig::class)
 class AuthControllerTest {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     lateinit var authService: AuthService
 
-    @MockBean
+    @MockitoBean
     lateinit var jwtService: JwtService
 
-    @MockBean
+    @MockitoBean
     lateinit var userDetailsService: UserDetailsServiceImpl
 
     @Test
--- core-api/src/test/kotlin/online/hanatalk/HealthControllerTest.kt
+++ core-api/src/test/kotlin/online/hanatalk/HealthControllerTest.kt
@@ -2,25 +2,28 @@ package online.hanatalk
 
 import online.hanatalk.api.HealthController
 import online.hanatalk.security.JwtService
+import online.hanatalk.security.SecurityConfig
 import online.hanatalk.security.UserDetailsServiceImpl
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
-@WebMvcTest(HealthController::class)
+@WebMvcTest(HealthController::class, excludeAutoConfiguration = [UserDetailsServiceAutoConfiguration::class])
+@Import(SecurityConfig::class)
 class HealthControllerTest {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    // Required by JwtAuthFilter which is loaded with the security config
-    @MockBean
+    @MockitoBean
     lateinit var jwtService: JwtService
 
-    @MockBean
+    @MockitoBean
     lateinit var userDetailsService: UserDetailsServiceImpl
 
     @Test
